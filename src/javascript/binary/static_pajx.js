ae73a1b7d8cedb52f4e581e3fa43b2500784f211
kelly <kelly@binary.com>
2017-01-20 12:10:13 +0800

    Update margin policy and contract specifications

--- src/javascript/binary/static_pajx.js
+++ src/javascript/binary/static_pajx.js
@@ -37,3 +37,24 @@ pjax_config_page("/terms-and-conditions", function() {
         }
     };
 });
+
+pjax_config_page("/contract-specifications", function() {
+   return {
+       onLoad: function() {
+           var hash;
+           function updateTab() {
+               hash = /^#(forex|volatility)-tab$/.test(window.location.hash) ? window.location.hash : '#forex-tab';
+               $('#cs-menu li').removeClass('active a-active');
+               $('.menu-has-sub-item div.toggle-content').addClass('invisible');
+               $(hash).addClass('active')
+                   .find('a').addClass('a-active');
+               $(hash + '-content').removeClass('invisible');
+           }
+           $(window).on('hashchange', function() {
+               updateTab();
+           });
+           updateTab();
+           $('.content-tab-container').removeClass('invisible');
+       }
+   };
+});
